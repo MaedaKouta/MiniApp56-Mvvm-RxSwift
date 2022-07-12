e18c8122e340a517ab423055fc7a7bacf5a83d45
Kota <maedakoutaa@gmail.com>
2022-07-12 09:47:34 +0900

    Initial Commit

---
+++ MiniApp56-Mvvm&RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp56-Mvvm&RxSwift
+//
+//  Created by 前田航汰 on 2022/07/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
